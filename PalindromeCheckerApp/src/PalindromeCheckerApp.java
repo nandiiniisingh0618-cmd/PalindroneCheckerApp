852e02bdb280a7dd04520dfb14db04c161e2c45d
sk8485-arch <sk8485@srmist.edu.in>
2026-02-23 14:42:16 +0530

    Your message

---
+++ PalindromeCheckerApp/src/PalindromeCheckerApp.java
@@ -0,0 +1,17 @@
+// 1. Class
+public class PalindromeCheckerApp {
+
+    // 2. Main Method & 3. Static Keyword
+    public static void main(String[] args) {
+
+        // 4. Console Output
+        System.out.println("=========================================");
+        System.out.println("  Welcome to the PalindromeChecker App!  ");
+        System.out.println("  Version: 1.0                           ");
+        System.out.println("=========================================");
+
+        // 5. Application Flow Control
+        // The program will either prompt the user for input here (UC2)
+        // or exit if there are no further instructions.
+    }
+}
